b4e3c21125750720f9b3f3b39b0b39cd74fdf099
unknown <maazanwer64@gmail.com>
2021-01-17 09:27:54 -0800

    practice

---
+++ app.js
@@ -0,0 +1,68 @@
+//CHANGING CASE
+// (1)
+// var info = prompt("What is your City's name?");
+// names = info.toUpperCase();
+// (2)
+// var info =prompt("What is your city's name?");
+// var first =info.slice(0,1);
+// first =first.toUpperCase();
+// var others =info.slice(1);
+// others =others.toLowerCase();
+// var total =first + others;
+
+
+//CONTROLLING THE LENGTH OF DECIMALS
+
+
+// var total = (30 / 45) * 100;
+// var fixedtotal = total.toFixed(2);
+// var actualtotal = ("$" + fixedtotal);
+
+// ROUNDING NUMBERS
+
+// var number = promhpt("Enter a positive integer(Supposedly an easy one if you would be so kind!!)")
+// alert(Math.round(number));
+// alert(Math.floor(number))
+// document.write(Math.ceil(number))
+
+// CHAP GENERATING RANDOM NUMBER
+
+// var random = Math.random();
+// alert(random)
+
+// var random =Math.ceil(Math.random() * 2) ;
+// alert(random)
+ 
+// var random =Math.floor(Math.random() * 10) + 1;
+// if(random === 5 || random === 6){
+//     alert("YOU have gotten the secret number")}
+
+// var random = prompt("Enter Your weight(Note only between 50 to 51 shall be entertained")
+// if(random > 50 && random < 50.5){
+//     alert("Well at least you entered correctly")
+// }else{alert("GET OUT!!! \n A person who doesnt understand such simple request is an automatic disqualify!")}
+// parseInt(random)
+// document.write(random)
+
+// Chap converting string to number and number to string
+
+// var a = "472"
+// var p =  parseInt(a)
+// document.write(p + 1)
+
+// var number = 35.36;
+// var s = number.toString();
+// var refnumber = s.replace(".", "")
+// document.write(refnumber)
+
+// CHAP STRINGS REPLACING CHARACTERS 
+
+// var a = "HYDERABAD"
+// var b = a.replace(/HYDER/ , "ISLAM");
+// document.write(b)
+
+// var a = "OKAY SO MY NAME IS XYZ AND I AM 19 AND I AM A COLLEGE STUDENT AND I UM YEAH..... THATS PRETTY MUCH IT...."
+// var b = a.replace(/AND/g , "&");
+// document.write(b)
+
+
